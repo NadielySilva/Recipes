ceda16e4d5154de6a4979c2546decb7cdf83735e
NadielySilva <nady.ceng@gmail.com>
2022-06-23 18:06:54 -0300

    working on header / assets added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-
+//import { useMediaQuery } from 'react-responsive';
+import { createGlobalStyle } from 'styled-components';
 
 //IMPORTING COMPONENTS
 import Header from './Components/Header';
@@ -8,11 +9,26 @@ import About from './Components/About';
 import Subscribe from './Components/Subscribe';
 import Footer from './Components/Footer';
 
+//IMPORTING ASSETS
+
+//FONTS
+// font-family: 'Roboto', sans-serif;
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`;
+
 export default class App extends Component{
+
 
   render(){
     return(
       <>
+        <GlobalStyle />
         <Header/>
         <Recipes/>
         <About/>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+//IMPORTING COMPONENTS
+import myHeaderImage from "../Assets/Header_Image_2x.png"
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+`;
+
+const Container = styled.div`
+    width: 100vw;
+    height: 130vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`;
+
+const HeaderBackground = styled.img`
+    width: 95vw;
+    height: 120vh;
+    object-fit: cover;
+`;
 
 export default function Header(){
 
     return(
         <>
-            <p>Esse é o Header</p>
+            <GlobalStyle />
+            <Container>
+                <HeaderBackground src={myHeaderImage}/>
+                <div>
+                    <p>RC</p>
+                </div>
+                <div>
+                    <button>About</button>
+                    <button>Recipes</button>
+                    <button>Subscribe</button>
+                </div>
+            </Container>
         </>
     )
 }
